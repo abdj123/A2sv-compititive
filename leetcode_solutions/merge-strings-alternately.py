6158cc792c6827acebce61be90d40057e6c4e4a1
Abeselom Dejene <84785093+abdj123@users.noreply.github.com>
2023-12-20 18:58:41 +0300

    Added solution for Merge Strings Alternately

---
+++ leetcode_solutions/merge-strings-alternately.py
@@ -0,0 +1,8 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+                return "".join(a + b for a, b in zip(word1, word2)) + word1[len(word2):]+word2[len(word1):]
+
+            
+        
+        
+        
