5867011f1ac44e90b28124c2b7cd64e6cf22bfe1
Abeselom Dejene <84785093+abdj123@users.noreply.github.com>
2024-01-02 17:28:17 +0300

    Added solution for Move Zeroes

--- leetcode_solutions/move-zeroes.py
+++ leetcode_solutions/move-zeroes.py
@@ -3,10 +3,10 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        slow = 0
-        for fast in range(len(nums)):
-            if nums[fast] != 0 and nums[slow] == 0:
-                nums[slow], nums[fast] = nums[fast], nums[slow]
-
-            if nums[slow] != 0:
-                slow += 1
+        
+        c=nums.count(0)
+        
+        for i in range(c):
+            nums.remove(0)
+        for i in range(c):
+            nums.append(0)
