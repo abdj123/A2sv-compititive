5362935b97e8f48b3eb9e54cd75e150592b16ccc
Abeselom Dejene <84785093+abdj123@users.noreply.github.com>
2024-02-27 14:19:03 +0300

    Add solution for Permutations

--- leetcode_solutions/leetcode/permutations.py
+++ leetcode_solutions/leetcode/permutations.py
@@ -1,7 +1,14 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
+        n=len(nums)
+        def backtrack(ind,path,cand):
+            if(len(path)==n):
+                res.append(path[:])
+                return
+            for i in range(0,len(cand)):
+                path.append(cand[i])
+                backtrack(i+1,path,cand[:i]+cand[i+1:])
+                path.pop()
         res=[]
-
-        for i in permutations(nums):
-            res.append(i)
+        backtrack(0,[],nums)
         return res
