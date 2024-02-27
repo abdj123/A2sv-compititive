ab95ad5119e52f2918f5cd21c569fb0aba3871d6
Abeselom Dejene <84785093+abdj123@users.noreply.github.com>
2024-02-27 11:58:43 +0300

    Add solution for Combinations

--- leetcode_solutions/leetcode/combinations.py
+++ leetcode_solutions/leetcode/combinations.py
@@ -1,19 +1,14 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        def comb(start):
+        def comb(indx,path):
             if(len(path)==k):
                 res.append(path[:])
-                return
-            if start > n:
-                return
-            path.append(start)
-            comb(start+1)
-            path.pop()
-            comb(start+1)
-
-            
+                return 
+            for i in range(indx,n+1):
+                path.append(i)
+                comb(i+1,path)
+                path.pop()
         res=[]
-        path=[]
-        comb(1)
-        return res
              
+        comb(1,[])
+        return res
