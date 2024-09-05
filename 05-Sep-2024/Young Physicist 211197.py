2ec64834c3c7e5b3d5bc3f109b2532e651ea2ee4
Abeselom Dejene <84785093+abdj123@users.noreply.github.com>
2024-09-05 09:37:13 +0300

    Created 'Young Physicist' by A2SV Hub

---
+++ 05-Sep-2024/Young Physicist 211197.py
@@ -0,0 +1,13 @@
+# Problem: Young Physicist - https://codeforces.com/problemset/problem/69/A
+
+n = int(input())
+x = y = z = 0
+for _ in range(n):
+    a, b, c = map(int, input().split())
+    x += a
+    y += b
+    z += c
+if x == y == z == 0:
+    print("YES")
+else:
+    print("NO")
