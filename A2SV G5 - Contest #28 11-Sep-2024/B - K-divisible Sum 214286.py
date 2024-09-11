c4a2f41cc782fcb6f7e0099c84ab7441e9c5a0ae
Abeselom Dejene <84785093+abdj123@users.noreply.github.com>
2024-09-11 11:53:54 +0300

    Created 'B - K-divisible Sum' by A2SV Hub

---
+++ A2SV G5 - Contest #28 11-Sep-2024/B - K-divisible Sum 214286.py
@@ -0,0 +1,8 @@
+# Problem: B - K-divisible Sum - https://codeforces.com/gym/540354/problem/B
+
+for _ in range(int(input())):
+    n,k = map(int,input().split())
+    if n >= k:
+        print(1 if n % k == 0 else 2)
+    else:
+        print((k-1)//n + 1)
