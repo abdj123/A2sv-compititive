9a87bce0d7f5f157f0487b74961e318ae6031207
Abeselom Dejene <84785093+abdj123@users.noreply.github.com>
2024-09-11 11:54:05 +0300

    Created 'C - Benches' by A2SV Hub

---
+++ A2SV G5 - Contest #28 11-Sep-2024/C - Benches 214287.py
@@ -0,0 +1,20 @@
+# Problem: C - Benches - https://codeforces.com/gym/540354/problem/C
+
+import sys
+from heapq import heapify, heappop, heappush
+input = lambda: sys.stdin.readline().rstrip()
+
+
+n = int(input())
+m = int(input())
+a = [int(input()) for line in range(n)]
+
+_max = max(a) + m
+
+heapify(a)
+for i in range(m):
+    min_bench = heappop(a)
+    heappush(a, min_bench + 1)
+
+_min = max(a)
+print(_min, _max)
