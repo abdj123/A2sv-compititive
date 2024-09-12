d2bf571509d0366a9c1bdaa85fc2fdc18006494a
Abeselom Dejene <84785093+abdj123@users.noreply.github.com>
2024-09-12 17:05:07 +0300

    Created 'Find Players With Zero or One Losses' by A2SV Hub

---
+++ 12-Sep-2024/Find Players With Zero or One Losses 214558.py
@@ -0,0 +1,17 @@
+# Problem: Find Players With Zero or One Losses - https://leetcode.com/problems/find-players-with-zero-or-one-losses
+
+class Solution:
+    def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
+        dc = defaultdict(lambda: [0, 0])
+        for a in matches:
+            dc[a[0]][0] += 1
+            dc[a[1]][1] += 1
+        ans = [[], []]
+        for a in dc:
+            if dc[a][1] == 0:
+                ans[0].append(a)
+            if dc[a][1] == 1:
+                ans[1].append(a)
+        ans[0].sort()
+        ans[1].sort()
+        return ans
