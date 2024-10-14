63b547b6a40b937fe1f0832fdbe79dc3088e70da
Abeselom Dejene <84785093+abdj123@users.noreply.github.com>
2024-10-14 17:58:12 +0300

    Created 'Tram' by A2SV Hub

---
+++ 14-Oct-2024/Tram 219941.py
@@ -0,0 +1,11 @@
+# Problem: Tram - https://codeforces.com/problemset/problem/116/A
+
+n = int(input())
+res = 0
+curr = 0
+for _ in range(n):
+    a, b = map(int, input().split())
+    curr -= a
+    curr += b
+    res = max(res, curr)
+print(res)
