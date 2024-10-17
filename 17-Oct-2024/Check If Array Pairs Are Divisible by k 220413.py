cfe12eedeafe9fbd5b9c72b6a0c5c98e79f8dca9
Abeselom Dejene <84785093+abdj123@users.noreply.github.com>
2024-10-17 20:53:03 +0300

    Created 'Check If Array Pairs Are Divisible by k' by A2SV Hub

---
+++ 17-Oct-2024/Check If Array Pairs Are Divisible by k 220413.py
@@ -0,0 +1,20 @@
+# Problem: Check If Array Pairs Are Divisible by k - https://leetcode.com/problems/check-if-array-pairs-are-divisible-by-k/
+
+class Solution(object):
+    def canArrange(self, arr, k):
+        freq = [0] * k
+        for num in arr:
+            rem = num % k
+            if rem < 0:
+                rem += k
+            freq[rem] += 1
+        
+        if freq[0] % 2 != 0:
+            return False
+        
+        for i in range(1, (k // 2) + 1):
+            if freq[i] != freq[k - i]:
+                return False
+        
+        return True
+        
