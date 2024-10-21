307f69c963aeb94f47197469507bcb5e7881988e
Abeselom Dejene <84785093+abdj123@users.noreply.github.com>
2024-10-21 19:48:59 +0300

    Created 'Long Pressed Name' by A2SV Hub

---
+++ 21-Oct-2024/Long Pressed Name 221197.py
@@ -0,0 +1,18 @@
+# Problem: Long Pressed Name - https://leetcode.com/problems/long-pressed-name/
+
+class Solution:
+    def isLongPressedName(self, name: str, typed: str) -> bool:
+        cnt = 0
+
+        answer = False
+        for i in range(len(typed)):
+            
+            if cnt < len(name) and name[cnt] == typed[i]:
+                cnt = cnt + 1
+            elif i == 0 or typed[i] != typed[i - 1]:
+                return answer
+
+        if cnt == len(name):
+            answer = True
+
+        return answer
