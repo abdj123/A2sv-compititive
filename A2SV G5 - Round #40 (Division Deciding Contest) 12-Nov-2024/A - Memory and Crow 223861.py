b68feba2a6135d346b5a97ea271b1bf5c503f733
Abeselom Dejene <84785093+abdj123@users.noreply.github.com>
2024-11-12 22:03:03 +0300

    Created 'A - Memory and Crow' by A2SV Hub

---
+++ A2SV G5 - Round #40 (Division Deciding Contest) 12-Nov-2024/A - Memory and Crow 223861.py
@@ -0,0 +1,12 @@
+# Problem: A - Memory and Crow - https://codeforces.com/gym/543431/problem/A
+
+n = int(input())
+ls = [int(i) for i in input().split()]
+
+res = [ls[-1]]
+ls.reverse()
+
+for i in range(1,n):
+    res.append(ls[i] + ls[i-1])
+
+print(*res[::-1])
