b680a42930fad17bd710218211534b7898336cc3
Abeselom Dejene <84785093+abdj123@users.noreply.github.com>
2024-11-12 22:01:03 +0300

    Created 'F - OR Encryption' by A2SV Hub

---
+++ A2SV G5 - Round #40 (Division Deciding Contest) 12-Nov-2024/F - OR Encryption 223860.py
@@ -0,0 +1,34 @@
+# Problem: F - OR Encryption - https://codeforces.com/gym/543431/problem/F
+
+import sys
+
+
+def solve():
+    n = int(sys.stdin.readline().strip())
+    matrix = []
+
+    for _ in range(n):
+        row = list(map(int, sys.stdin.readline().strip().split()))
+        matrix.append(row)
+
+    a = [2**30 - 1] * n
+    
+    for i in range(n):
+        for j in range(n):
+            if i != j:
+                a[i] &= matrix[i][j]
+                a[j] &= matrix[i][j]
+
+    for i in range(n):
+        for j in range(n):
+            if i != j and a[i] | a[j] != matrix[i][j]:
+                print("NO")
+                return
+            
+    print("YES")
+    print(*a)
+    
+t = int(sys.stdin.readline().strip())
+
+for _ in range(t):
+    solve()
