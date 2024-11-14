1d1ee67c291e8dbf5913e4fbf634b8a1ea08805c
Abeselom Dejene <84785093+abdj123@users.noreply.github.com>
2024-11-14 07:36:03 -0800

    Created 'C - Avoid Trygub' by A2SV Hub

---
+++ A2SV G5 - Round #40 (Division Deciding Contest) 14-Nov-2024/C - Avoid Trygub 223918.py
@@ -0,0 +1,12 @@
+# Problem: C - Avoid Trygub - https://codeforces.com/gym/543431/problem/C
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    s = input()
+
+    ls = [i for i in s]
+    ls.sort()
+
+    print("".join(ls))
