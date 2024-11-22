f5d3fa1bc94b3755eb8e11546df438325a4554bf
Abeselom Dejene <84785093+abdj123@users.noreply.github.com>
2024-11-22 18:30:21 +0300

    Created 'D - Decide Your Division' by A2SV Hub

---
+++ A2SV G5 - Round #40 (Division Deciding Contest) 22-Nov-2024/D - Decide Your Division 224247.py
@@ -0,0 +1,26 @@
+# Problem: D - Decide Your Division - https://codeforces.com/gym/543431/problem/D
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    two = three = five = 0
+
+    while n % 2 == 0:
+        n //= 2
+        two += 1
+
+    while n % 3 == 0:
+        n //= 3
+        three += 1
+
+    while n % 5 == 0:
+        n //= 5
+        five += 1
+        
+    if n != 1:
+        print(-1)
+    else:
+        print(two + 2 * three + 3 * five)
+    
+
